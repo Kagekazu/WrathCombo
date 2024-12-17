3af9a3c299de3009a758854b1705796ce5fad048
Kage <95875972+Kagekazu@users.noreply.github.com>
2024-12-17 09:58:00 +0100

    cleanup

--- WrathCombo/CustomCombo/Functions/Movement.cs
+++ WrathCombo/CustomCombo/Functions/Movement.cs
@@ -1,28 +1,30 @@
-﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+﻿#region
+
 using System;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using WrathCombo.Services;
 
-namespace WrathCombo.CustomComboNS.Functions
+#endregion
+
+namespace WrathCombo.CustomComboNS.Functions;
+
+internal abstract partial class CustomComboFunctions
 {
-    internal abstract partial class CustomComboFunctions
+    private static DateTime? movementStarted;
+
+    public static TimeSpan TimeMoving => movementStarted is null ? TimeSpan.Zero : DateTime.Now - movementStarted.Value;
+
+    /// <summary> Checks player movement </summary>
+    public static unsafe bool IsMoving()
     {
-        private static DateTime? movementStarted;
+        bool isMoving = AgentMap.Instance() is not null && AgentMap.Instance()->IsPlayerMoving > 0;
 
-        /// <summary> Checks player movement </summary>
-        public static unsafe bool IsMoving()
-        {
-            bool isMoving = AgentMap.Instance() is not null && AgentMap.Instance()->IsPlayerMoving > 0;
+        if (isMoving && movementStarted is null)
+            movementStarted = DateTime.Now;
 
-            if (isMoving && movementStarted is null)
-                movementStarted = DateTime.Now;
+        if (!isMoving)
+            movementStarted = null;
 
-            if (!isMoving)
-                movementStarted = null;
-
-
-            return isMoving && (TimeMoving.TotalMilliseconds / 1000f) >= Service.Configuration.MovementLeeway;
-        }
-
-        public static TimeSpan TimeMoving => movementStarted is null ? TimeSpan.Zero : (DateTime.Now - movementStarted.Value);
+        return isMoving && TimeMoving.TotalMilliseconds / 1000f >= Service.Configuration.MovementLeeway;
     }
 }
